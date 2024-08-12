04f981a18749e036a4b11d732d9416a488daed2e
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2024-08-12 17:26:35 +0300

    Update DataSection.jsx

--- app/components/DataSection.jsx
+++ app/components/DataSection.jsx
@@ -1,12 +1,24 @@
-// components/DataSection.jsx
+import React from 'react';
+
 function DataSection({ title, value }) {
-    return (
-      <div>
-        <h2>{title}</h2>
-        <p>{value}</p>
-      </div>
-    );
-  }
-  
-  export default DataSection;
-  
+  return (
+    <div className="my-4">
+      <h2>{title}</h2>
+      <p>{value}</p>
+    </div>
+  );
+}
+
+function CountryDataSections({ selectedCountry, totalMedals, ratio, rank }) {
+  if (!selectedCountry) return null;
+
+  return (
+    <div>
+      <DataSection title="Total Medals:" value={totalMedals} />
+      {/* <DataSection title="Medals per Person" value={ratio !== null ? ratio : 'Loading...'} /> */}
+      <DataSection title="Medal rank in relation to population size:" value={rank !== null ? rank : 'Loading...'} />
+    </div>
+  );
+}
+
+export default CountryDataSections;
