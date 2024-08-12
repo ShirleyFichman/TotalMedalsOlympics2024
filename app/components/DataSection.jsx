f56acc43e6d54665f5ba23421c40ebeeba580451
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2024-08-12 15:28:26 +0300

    Add files via upload

---
+++ app/components/DataSection.jsx
@@ -0,0 +1,12 @@
+// components/DataSection.jsx
+function DataSection({ title, value }) {
+    return (
+      <div>
+        <h2>{title}</h2>
+        <p>{value}</p>
+      </div>
+    );
+  }
+  
+  export default DataSection;
+  
