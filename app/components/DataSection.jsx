35b7821ec7ebecb233c3d66eb9f176776156191e
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2024-08-13 11:37:21 +0300

    change margin

--- app/components/DataSection.jsx
+++ app/components/DataSection.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 
 function DataSection({ title, value }) {
   return (
-    <div className="my-4">
+    <div className="my-6">
       <h2>{title}</h2>
       <p>{value}</p>
     </div>
   );
 }
 
-function CountryDataSections({ selectedCountry, totalMedals, ratio, rank }) {
+function CountryDataSections({ selectedCountry, totalMedals, rank }) {
   if (!selectedCountry) return null;
 
   return (
     <div>
       <DataSection title="Total Medals:" value={totalMedals} />
-      {/* <DataSection title="Medals per Person" value={ratio !== null ? ratio : 'Loading...'} /> */}
       <DataSection title="Medal rank in relation to population size:" value={rank !== null ? rank : 'Loading...'} />
     </div>
   );
