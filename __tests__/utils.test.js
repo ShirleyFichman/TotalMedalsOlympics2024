18b46d5f2ff33174d9cb35fece6a5c52c434720d
Shirley Fichman <69894229+ShirleyFichman@users.noreply.github.com>
2024-08-21 11:48:16 +0300

    adding Country and Population Data Matching test

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,7 +1,6 @@
 // __tests__/utils.test.js
 
-import { parseCSV } from '../utils/utils';
-
+import { parseCSV, fetchMedalsTotalCSV, fetchPopulationCSV } from '../utils/utils';
 
 describe('parseCSV', () => {
   it('should parse CSV data correctly', () => {
@@ -23,5 +22,39 @@ describe('parseCSV', () => {
     const result = parseCSV(csvData);
 
     expect(result).toEqual(expectedResult);
+  });
+});
+
+describe('Country and Population Data Matching', () => {
+  let countries;
+  let populationData;
+
+  beforeAll(async () => {
+    try {
+      [countries, populationData] = await Promise.all([
+        fetchMedalsTotalCSV(),
+        fetchPopulationCSV(),
+      ]);
+      expect(countries).toBeDefined();
+      expect(populationData).toBeDefined();
+      expect(countries.length).toBeGreaterThan(0);
+      expect(populationData.length).toBeGreaterThan(0);
+      console.log("FINISH BEFOREALL");
+    } catch (error) {
+      console.error('Error fetching CSV data:', error);
+    }
+  });
+
+  it('should find a matching population entry for every country in the countries data', () => {
+    countries.forEach((country) => {
+      const matchingPopulation = populationData.find(
+        (item) => item.CCA3 === country.country_code
+      );
+
+      if (!matchingPopulation) {
+        console.log(`No matching population found for country_code: ${country.country_code}`);
+        expect(matchingPopulation).toBeTruthy();
+      }
+    });
   });
 });
